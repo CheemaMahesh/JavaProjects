6a9412765b91c1593703fce62de8c7dd79a6a46b
CheemaMahesh <124231713+CheemaMahesh@users.noreply.github.com>
2023-06-22 02:20:25 +0530

    Add files via upload

---
+++ mahi.java
@@ -0,0 +1,19 @@
+package average;
+import java.util.Scanner;
+public class mahi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+String a;
+int m1,m2,m3;
+a=s.next();
+m1=s.nextInt();
+m2=s.nextInt();
+m3=s.nextInt();
+int b=(m1+m2+m3)/3;
+System.out.println(a);
+System.out.println(b);
+	}
+
+}
